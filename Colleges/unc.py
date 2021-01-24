e38af24d249cb461f6b018d735afb90bcd1d55c9
gaganj007 <67553802+gaganj007@users.noreply.github.com>
2021-01-23 22:51:22 -0500

    Update unc.py

--- Colleges/unc.py
+++ Colleges/unc.py
@@ -1,92 +1,61 @@
 # List classes and demographics of each class for this college
-
 import csv
 
 f = open("test_python.csv")
 
 csv_f = csv.reader(f)
 
-
-## Variables to hold the values from the csv file
-collegeName = []
-gender = []
-courseName = []
-race = []
-
-## Appending the info from the file
+raceCvs = []
+courseNameCvs = []
+genderCvs = []
+courseThing =""
 for row in csv_f:
-    collegeName.append(row[1])
-    gender.append(row[2])
-    race.append(row[3])
-    courseName.append(row[4])
-
-## Gets rid of the question that was on the google form
-collegeName = collegeName[1::]
-gender = gender[1::]
-race = race[1::]
-courseName= courseName[1::]
-originalCourseName =[]
-modifiedCourseName = []
-
-## Makes a list of courses that each students has taken by seperating them by ","
-for i in courseName:
-    originalCourseName.append(i.split(", "))
+    raceCvs.append(row[3])
+    courseThing += row[4] + ' '
+    genderCvs.append(row[2])
+    courseNameCvs.append(row[4])
 
 
-## Combines all the subjects of each students into one list to be used later
-def printOutCourse(originalCoure):
-    for i in range(0,len(originalCoure)):
-        for j in range(0,len(originalCoure[i])):
-            toPrint = originalCourseName[i][j]
-            modifiedCourseName.append(toPrint.upper())
-    print(modifiedCourseName)
 
-##Checks for duplicate course and skips that 
-courseWithoutDuplicate = []
-def getRidOfDuplicate(allCourseList):
-    for i in allCourseList:
-        if i not in courseWithoutDuplicate:
-            courseWithoutDuplicate.append(i)
+def race_counter(course: str, race: str) -> float:
+    z: int = 0
 
-    return courseWithoutDuplicate
-    print(courseWithoutDuplicate)
+    for i in range(0,len(courseNameCvs)):
+        if(course in courseNameCvs[i]):
+            if(race == raceCvs[i]):
+                z += 1
+    return z
 
-
-printOutCourse(originalCourseName)
-getRidOfDuplicate(modifiedCourseName)
+Black: str = "Black or African American"
+Asian: str = "Asian or Pacific-Islander"
+White: str = "White/Caucasian"
+Hispanic: str = "Hispanic/Latinx"
+Middle: str = "Middle-Eastern/North African"
+Native: str = "American Indian or Alaska Native"
 
 
 
 
-## Finds the indexes at which the course overalp with other students (it's not doing that tho)
-def lookForIndex(courseWithoutDuplicate, originalList):
-    index = []
-    indexY = []
-    shortIndex = []
-    shortInedxY = []
-    for i in range(0,len(courseWithoutDuplicate)):
-        for j in range(0,len(originalList)):
-            for k in range(0,len(originalList[j])):
-                if(courseWithoutDuplicate[i] == originalList[j][k]):
-                    index.append(j)
-                    indexY.append(k)
-                else:
-                    continue
-
-                for a in index:
-                    if a not in shortIndex:
-                        shortIndex.append(a)
-                for b in indexY:
-                    if b not in shortInedxY:
-                        shortInedxY.append(b)
-    print(index)
-    print(indexY)
-    print(shortIndex)
-    print(shortInedxY)
-
-lookForIndex(courseWithoutDuplicate,originalCourseName)
+def specific_race(course: str) -> dict[str, float]:
+    x: float = race_counter(course, Black)
+    y: float = race_counter(course, Asian)
+    z: float = race_counter(course, White)
+    n: float = race_counter(course, Hispanic)
+    m: float = race_counter(course, Middle)
+    u: float = race_counter(course, Native)
+    race_list = [Black, Asian, White, Hispanic, Middle, Native]
+    race_counter_List = [x, y, z, n, m, u]
+    total: float = x + y + z + n + m + u
+    o: dict[str, float] = {}
+    j: int = 0
+    while j < len(race_list):
+        sum: float = race_counter_List[j]/total
+        o[race_list[j]] = sum
+        j += 1
+    return o
 
 
+print(specific_race("MATH 381"))
 
 
 
